ab214c834824ffb48d4d9391119eedaec68fa662
redhendery <redhendery@gmail.com>
2021-01-04 11:44:54 -0800

    update mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,8 @@ Rails.application.configure do
   host = 'https://marincricketclub.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
+    user_name: 'apikey',
+    password: ENV['SENDGRID_API_KEY'],
     domain: 'marincricketclub.com',
     address: 'smtp.sendgrid.net',
     port: 587,

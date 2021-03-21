13c93a0ea318a0628e579452ab3c00a23e7beef4
redhendery <redhendery@gmail.com>
2021-03-21 14:53:41 -0700

    revert to sendgrid for mail send

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,18 +63,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "marin_website_production"
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'marincricketclub.com'
   config.action_mailer.default_url_options = { host: host }
 
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-    :address              => 'smtp.gmail.com',
-    :port                 => 587,
-    :user_name            => ENV['GMAIL_USERNAME'],
-    :password             => ENV['GMAIL_PASSWORD'],
-    :authentication       => 'plain',
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => 'apikey',
+    :password       => ENV['SENDGRID_API_KEY'],
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 

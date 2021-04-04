d826cb91c00a557785474eb9cdb4ffe46d10c297
redhendery <redhendery@gmail.com>
2021-04-04 09:00:14 -0700

    update mail config for the 1000th time

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'marincricketclub.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'marincricketclub.com' }
 
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',

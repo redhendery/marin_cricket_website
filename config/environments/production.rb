034e280688607d901d41d436421f7b18250fcaef
redhendery <redhendery@gmail.com>
2021-06-07 09:52:31 -0700

    add www. to correct link issue in automated emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { protocol: 'https', host: 'marincricketclub.com' }
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'www.marincricketclub.com' }
 
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',

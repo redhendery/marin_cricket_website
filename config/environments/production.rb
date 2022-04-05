c46dbef3dd4902d6a03a10ef291715feaf167eda
redhendery <redhendery@gmail.com>
2022-04-04 17:20:59 -0700

    change mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { protocol: 'https', host: 'www.marincricketclub.com' }
 
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,

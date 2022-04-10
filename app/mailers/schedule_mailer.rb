c56cd306180c68133211a51dc9f01a8413782020
redhendery <redhendery@gmail.com>
2022-04-10 12:03:42 -0700

    update email templates

--- app/mailers/schedule_mailer.rb
+++ app/mailers/schedule_mailer.rb
@@ -2,8 +2,8 @@ class ScheduleMailer < ApplicationMailer
   def upcoming_games_newsletter(newsletter, upcoming)
     @newsletter = newsletter
     @upcoming = Schedule.where(date: Date.current..(Date.current + 7.days))
-    @mno = @upcoming.where(home_team_id: 1).or(@upcoming.where(away_team_id: 1))
-    @mnt = @upcoming.where(home_team_id: 2).or(@upcoming.where(away_team_id: 2))
+    @mnb = @upcoming.where(home_team_id: 39).or(@upcoming.where(away_team_id: 39))
+    @mnc = @upcoming.where(home_team_id: 1).or(@upcoming.where(away_team_id: 1))
     @mns = @upcoming.where(home_team_id: 3).or(@upcoming.where(away_team_id: 3))
     mail to: newsletter.email, subject: 'Marin Cricket Club Games This Weekend!'
   end
@@ -11,8 +11,8 @@ class ScheduleMailer < ApplicationMailer
   def upcoming_games_email(user, upcoming)
     @user = user
     @upcoming = Schedule.where(date: Date.current..(Date.current + 7.days))
-    @mno = @upcoming.where(home_team_id: 1).or(@upcoming.where(away_team_id: 1))
-    @mnt = @upcoming.where(home_team_id: 2).or(@upcoming.where(away_team_id: 2))
+    @mnb = @upcoming.where(home_team_id: 39).or(@upcoming.where(away_team_id: 39))
+    @mnc = @upcoming.where(home_team_id: 1).or(@upcoming.where(away_team_id: 1))
     @mns = @upcoming.where(home_team_id: 3).or(@upcoming.where(away_team_id: 3))
     mail to: user.email, subject: 'Marin Cricket Club Games This Weekend!'
   end

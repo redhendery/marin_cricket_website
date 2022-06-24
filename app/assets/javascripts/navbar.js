b2e62b795f12c9345844781d88870bd01bebd0f7
redhendery <redhendery@gmail.com>
2022-06-24 09:45:13 -0700

    attempt tp correct build error

--- app/assets/javascripts/navbar.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  var burger = document.querySelector('.burger');
-  var nav = document.querySelector('#'+burger.dataset.target);
-  burger.addEventListener('click', () => {
-    burger.classList.toggle('is-active');
-    nav.classList.toggle('is-active');
-  });
-})();
